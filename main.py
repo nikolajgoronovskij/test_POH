7fc22d648993b650be29b0edb1b9458e707566b3
lakustovdimka <lakustovdimka@gmail.com>
2025-08-10 02:15:07 +0500

    12345

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 
 
 def print_hi(name):
-    # 1234 Use a breakpoint in the code line below to debug your script.
+    # 12345 Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
